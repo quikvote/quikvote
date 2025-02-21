cc03f221a35f010c8816e89c32745852a73c69c2
Matthew Nelson <matthewbuzz@gmail.com>
2025-02-21 13:30:36 -0700

    Fix typo

--- service/database/RoomDAO.ts
+++ service/database/RoomDAO.ts
@@ -1,5 +1,5 @@
 import { WithId } from "mongodb";
-import { Room, Vote } from "../model";
+import { Room, Votes } from "../model";
 
 export interface RoomDAO {
     createRoom: (creatorUsername: string) => Promise<WithId<Room>>,
@@ -7,7 +7,7 @@ export interface RoomDAO {
     getRoomById: (roomId: string) => Promise<WithId<Room> | null>,
     addParticipantToRoom: (roomCode: string, username: string) => Promise<boolean>,
     addOptionToRoom: (roomId: string, option: string) => Promise<boolean>,
-    submitUserVotes: (roomId: string, username: string, votes: Vote) => Promise<boolean>,
+    submitUserVotes: (roomId: string, username: string, votes: Votes) => Promise<boolean>,
     removeUserVotes: (roomId: string, username: string) => Promise<void>,
     closeRoom: (roomId: string) => Promise<boolean>,
     deleteRoom: (roomId: string) => Promise<boolean>
