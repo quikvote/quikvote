cc7eab4af11a7fb6465d00224227ee0a446b2779
Austin Hillyard <austin.hillyard@gmail.com>
2025-02-21 21:40:39 -0700

    Separated out some of the functionality from caluclateVoteResult.ts into separate component functions.

--- service/calculateVoteResult.ts
+++ service/calculateVoteResult.ts
@@ -1,16 +1,24 @@
 import { UserVote } from "./model";
 
 const calculateVoteResult = (userVotes: UserVote[]) => {
+    return sortOptions(calculateTotals(userVotes));
+}
+
+const calculateTotals = (userVotes: UserVote[]) => {
     const totals = new Map()
     userVotes.forEach(userVote => {
         Object.keys(userVote.votes).forEach(key => {
             totals.set(key, (totals.get(key) ?? 0) + userVote.votes[key])
         })
     });
+    return totals
+}
+
+const sortOptions = (totals: Map<any,  any>) => {
     const sortedOptions = Array.from(totals)
         .sort((a, b) => b[1] - a[1])
         .map(([key]) => key)
     return sortedOptions
 }
 
-export default calculateVoteResult;
+export default calculateVoteResult; calculateTotals; sortOptions;
