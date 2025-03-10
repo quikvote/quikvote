16b2c40e13f6fc06766860530b0d8ac20f70fe2a
Josh Hendershot <hendershotjoshr@gmail.com>
2025-03-09 22:44:30 -0600

    chore: formatting

--- service/model/index.ts
+++ service/model/index.ts
@@ -1,32 +1,33 @@
 import { VoteConfig, Vote } from "./voteTypes"
 
 export interface User {
-    username: string
-    nickname: string
-    password: string
-    token: string
-    timestamp: number
+  username: string
+  nickname: string
+  password: string
+  token: string
+  timestamp: number
 }
 
 export interface UserVote {
-    username: string
-    vote: Vote
+  username: string
+  vote: Vote
 }
 
 export interface Room {
-    code: string
-    owner: string
-    state: 'open' | 'closed'
-    participants: string[]
-    options: string[]
-    votes: UserVote[]
-    config: VoteConfig
-    timestamp: number
+  code: string
+  owner: string
+  state: 'open' | 'closed'
+  participants: string[]
+  options: string[]
+  votes: UserVote[]
+  config: VoteConfig
+  timestamp: number
 }
 
 export interface Result {
-    owner: string
-    winner: string
-    details: Record<string, number>
-    timestamp: number
+  owner: string
+  winner: string
+  details: Record<string, number>
+  // room_id: string
+  timestamp: number
 }
