589033fbcab46a2c268baacd52823af66e2da463
Josh Hendershot <hendershotjoshr@gmail.com>
2025-03-10 18:07:35 -0600

    refactor: use close ws event and remove endpoint

--- service/index.ts
+++ service/index.ts
@@ -124,25 +124,25 @@ async function main() {
     const roomId = req.params.id
     const room = await roomDAO.getRoomById(roomId);
 
-        if (!user) {
-            res.status(404).send({ msg: `User not signed in` })
-            return
-        }
+    if (!user) {
+      res.status(404).send({ msg: `User not signed in` })
+      return
+    }
 
-        if (!room) {
-            res.status(404).send({ msg: `Room ${roomId} does not exist` })
-            return
-        }
+    if (!room) {
+      res.status(404).send({ msg: `Room ${roomId} does not exist` })
+      return
+    }
 
     if (room.state !== 'open') {
       res.status(409).send({ msg: 'Room is not open' })
       return
     }
 
-        await roomDAO.addParticipantToRoom(room.code, user.username);
+    await roomDAO.addParticipantToRoom(room.code, user.username);
 
-        res.status(200).send({ ...room, isOwner: room.owner === user.username })
-    })
+    res.status(200).send({ ...room, isOwner: room.owner === user.username })
+  })
 
   anonymousApiRouter.post('/room/:code/join', async (req: Request, res: Response) => {
     const user = await getUserFromRequest(req)
@@ -184,35 +184,6 @@ async function main() {
     const user = await getUserFromRequest(req)
     const newRoom = await roomDAO.createRoom(user!.username);
     res.status(201).send({ id: newRoom._id, code: newRoom.code })
-  })
-
-  secureApiRouter.post('/room/:id/close', async (req: Request, res: Response) => {
-    const user = await getUserFromRequest(req)
-    const roomId = req.params.id
-    const room = await roomDAO.getRoomById(roomId);
-
-    if (!room) {
-      res.status(404).send({ msg: `Room ${roomId} does not exist` })
-      return
-    }
-    const isOwner = room.owner === user!.username
-
-    if (!isOwner) {
-      res.status(403).send({ msg: 'User is not owner of room' })
-      return
-    }
-
-    if (room.state !== 'open') {
-      res.status(409).send({ msg: 'Room is not open' })
-      return
-    }
-
-    await roomDAO.closeRoom(roomId);
-
-    const { sortedOptions, sortedTotals } = calculateVoteResult(room.votes)
-    const result = await historyDAO.createResult(user!.username, sortedOptions, sortedTotals);
-
-    res.status(200).send({ resultsId: result._id })
   })
 
   secureApiRouter.get('/results/:id', async (req: Request, res: Response) => {
