e4bacf0c29173ce23524bade823bfa7bc7a61608
Josh Hendershot <hendershotjoshr@gmail.com>
2025-03-10 19:35:12 -0600

    fix: use config from room request

--- service/database/RoomDAO.ts
+++ service/database/RoomDAO.ts
@@ -1,14 +1,15 @@
 import { WithId } from "mongodb";
-import { Room, Votes } from "../model";
+import { Room } from "../model";
+import { Vote, VoteConfig } from "../model/voteTypes";
 
 export interface RoomDAO {
-    createRoom: (creatorUsername: string) => Promise<WithId<Room>>,
-    getRoomByCode: (roomCode: string) => Promise<WithId<Room> | null>,
-    getRoomById: (roomId: string) => Promise<WithId<Room> | null>,
-    addParticipantToRoom: (roomCode: string, username: string) => Promise<boolean>,
-    addOptionToRoom: (roomId: string, option: string) => Promise<boolean>,
-    submitUserVotes: (roomId: string, username: string, votes: Votes) => Promise<boolean>,
-    removeUserVotes: (roomId: string, username: string) => Promise<void>,
-    closeRoom: (roomId: string) => Promise<boolean>,
-    deleteRoom: (roomId: string) => Promise<boolean>
+  createRoom: (creatorUsername: string, config: VoteConfig) => Promise<WithId<Room>>,
+  getRoomByCode: (roomCode: string) => Promise<WithId<Room> | null>,
+  getRoomById: (roomId: string) => Promise<WithId<Room> | null>,
+  addParticipantToRoom: (roomCode: string, username: string) => Promise<boolean>,
+  addOptionToRoom: (roomId: string, option: string) => Promise<boolean>,
+  submitUserVotes: (roomId: string, username: string, vote: Vote) => Promise<boolean>,
+  removeUserVotes: (roomId: string, username: string) => Promise<void>,
+  closeRoom: (roomId: string) => Promise<boolean>,
+  deleteRoom: (roomId: string) => Promise<boolean>
 }
